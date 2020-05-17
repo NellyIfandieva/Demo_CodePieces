1ba9395b354cd837a767f0c7c09505c8e321246a
nelly Ifandieva <n.ifandieva@gmail.com>
2020-05-17 22:25:29 +0300

    Added the ArticleConstants file for reference

---
+++ ArticleConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace Blog.Test.TestConstants.ArticleServiceTestConstants
+{
+    public static class ArticleConstants
+    {
+        public static object GetArticlesNum 
+            => 3;
+        public static string IsByUserTrueDbName 
+            => "ArticleIsByUserExists";
+        public static string IsByUserFalseDbName 
+            => "ArticleIsByUserDoesNotExist";
+        public static string IsByUserFalseUserId 
+            => "User";
+    }
+}
