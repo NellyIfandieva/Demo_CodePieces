d92a9f8f06fa2fcf3a331d4f7c0a64d7df350201
Nelly Ifandieva <nelly_vj@yahoo.com>
2020-05-17 19:15:53 +0300

    Renamed the FakeDbContextFactory, to FakeArticlesFactory

---
+++ FakeArticlesFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace Blog.Test.Fakes
+{
+    using Blog.Data.Models;
+
+    public static class FakeArticlesFactory
+    {
+        public static Article[] CreateArticles(int articlesNum)
+        {
+            Article[] articles = new Article[articlesNum];
+
+            for (int i = 0; i < articles.Length; i++)
+            {
+                articles[i] = new Article
+                {
+                    Id = i,
+                    UserId = $"{i}"
+                };
+            }
+
+            return articles;
+        }
+    }
+}
